56608d54671d355211b29ef1bf54ca85865be745
Alexey <alexey.stadnic.job@gmail.com>
2022-08-29 20:00:54 +0300

    TaskListDictionary-2 fix: fix bugs

--- PracticeWithListDictionary/Program.cs
+++ PracticeWithListDictionary/Program.cs
@@ -15,31 +15,21 @@ class Program
        
         string searchPhone = clients[999].Phone; 
         Stopwatch stopWatch = new Stopwatch();
+        Client client = new Client();
         
         stopWatch.Start();
-        foreach (var client in clients)
-        {
-            if (searchPhone.Equals(client.Phone))
-            {
-                stopWatch.Stop();
-                Console.WriteLine("Поиск в списке завершен. Имя клиента " + client.Name + 
-                                  ", номер телефона " + client.Phone);
-                Console.WriteLine("Время поиска " + stopWatch.ElapsedTicks);
-            }
-        }
-      
+        client = clients.FirstOrDefault(c => c.Phone.Equals(searchPhone));
+        stopWatch.Stop();
+        Console.WriteLine("Поиск в списке завершен. Имя клиента " + client.Name + 
+                          ", номер телефона " + client.Phone);
+        Console.WriteLine("Время поиска " + stopWatch.ElapsedTicks);
+
         stopWatch.Restart();
-        foreach (var client in clientsInDictionary)
-        {
-            if (searchPhone.Equals(client.Key))
-            {
-                stopWatch.Stop();
-                Console.WriteLine("Поиск в словаре завершен. Имя клиента " + client.Value.Name + 
-                                  ", номер телефона " + client.Key);
-                Console.WriteLine("Время поиска " + stopWatch.ElapsedTicks);
-            }
-                
-        }
+        client = clientsInDictionary[searchPhone];
+        stopWatch.Stop();
+        Console.WriteLine("Поиск в словаре по ключу завершен. Имя клиента " + client.Name + 
+                          ", номер телефона " + client.Phone);
+        Console.WriteLine("Время поиска " + stopWatch.ElapsedTicks);
         
         stopWatch.Restart();
         var searchClient = clientsInDictionary.FirstOrDefault(c => c.Key.Equals(searchPhone));
@@ -48,43 +38,13 @@ class Program
                           ", номер телефона " + searchClient.Key);
         Console.WriteLine("Время поиска " + stopWatch.ElapsedTicks);
         
-        SelectClientsBelowAge(clients,35);
+        // выбираем клиентов младше 35 лет
+        clients.Where(c => (DateTime.Today.Year - c.Birthday.Year) < 35).ToList();
         
-        Employee employee = SelectEmployeeWithMinSalary(employees);
-        Console.WriteLine("Сотрудник с минимальной зарплатой " + employee.Name);
-        Console.WriteLine("Зарплата составляет " + employee.Salary);
-
-        
-        void SelectClientsBelowAge(List<Client> clients, int age)
-        {
-            int count = 0;
-            Console.WriteLine("Клиенты младше " + age);
-            foreach (Client client in clients)
-            {
-                if (DateTime.Today.Year - client.Birthday.Year < age)
-                {
-                    Console.WriteLine(client.Name + ", " + client.Birthday.Year + " года рождения");
-                    count++;
-                }
-            }
-            Console.WriteLine("Всего таких сотрудников " + count);
-        }
-
-        Employee SelectEmployeeWithMinSalary(List<Employee> employees)
-        {
-            int minSalary = Int32.MaxValue;
-            Employee searchEmployee = new Employee();
-            
-            foreach (Employee employee in employees)
-            {
-                if (minSalary > employee.Salary)
-                {
-                    minSalary = employee.Salary;
-                    searchEmployee = employee;
-                }
-            }
-            
-            return searchEmployee;
-        }
+        // находим сотрудника с минимальной зарплатой
+        int minSalary = employees.Min(c => c.Salary);
+        Employee employeeWithMinSalary = employees.First(c => c.Salary == minSalary);
+        Console.WriteLine("Сотрудник с минимальной зарплатой " + employeeWithMinSalary.Name);
+        Console.WriteLine("Зарплата составляет " + employeeWithMinSalary.Salary);
     }
 }
