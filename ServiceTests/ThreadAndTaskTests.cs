de0e1ebdfb2de2aa706ed5eab8c3db261d3f017b
Alexey <alexey.stadnic.job@gmail.com>
2022-10-09 16:51:00 +0300

    TaskThread-0 feat: add ThreadAndTaskTests

---
+++ ServiceTests/ThreadAndTaskTests.cs
@@ -0,0 +1,55 @@
+﻿using Models;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ServiceTests
+{
+    public class ThreadAndTaskTests
+    {
+        private ITestOutputHelper _output;
+
+        public Account account = new Account();
+        public Currency currency = new Currency();
+
+        public Object locker = new Object();
+        
+        public ThreadAndTaskTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void AccrualMoneyPositiveTest()
+        {
+            currency.Name = "USD";
+            currency.Code = 840;
+            account.Currency = currency;
+            account.Amount = 0;
+
+            var flowOne = new Thread(AccrualMoney);
+            flowOne.Name = "flowOne";
+            var flowTwo = new Thread(AccrualMoney);
+            flowTwo.Name = "flowTwo";
+            
+            flowOne.Start();            
+            flowTwo.Start();
+
+            Thread.Sleep(50000);
+            Assert.True(true);            
+        }
+
+        void AccrualMoney()
+        {
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                lock (locker)
+                {
+                    account.Amount += 100;
+                }
+                _output.WriteLine($"{Thread.CurrentThread.Name}: {account.Amount}");
+            }
+        }
+    }
+}
