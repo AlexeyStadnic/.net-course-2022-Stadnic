11002d9cf99c39fefd927bef21fb14529f9ef685
Alexey <alexey.stadnic.job@gmail.com>
2022-10-23 20:01:03 +0300

    TaskHttpClient-0 feat: add CurrencyService

---
+++ Services/CurrencyService.cs
@@ -0,0 +1,23 @@
+﻿using Models;
+using Newtonsoft.Json;
+
+namespace Services
+{
+    public class CurrencyService
+    {        
+        public async Task<CurrencyResponce> CurrencyConversion(Currency from, Currency to, int amount)
+        {
+            CurrencyResponce currencyResponce;
+            using (var client = new HttpClient())
+            {
+                HttpResponseMessage responseMessage = await 
+                    client.GetAsync($"https://www.amdoren.com/api/currency.php?api_key=nQpJHTHkKqHLkiqRmrq7nEXTRGmgtf" +
+                    $"&from={from.Name}&to={to.Name}&amount={amount}");
+                responseMessage.EnsureSuccessStatusCode();
+                string message = await responseMessage.Content.ReadAsStringAsync();
+                currencyResponce = JsonConvert.DeserializeObject<CurrencyResponce>(message);
+            }
+            return currencyResponce;
+        }
+    }
+}
